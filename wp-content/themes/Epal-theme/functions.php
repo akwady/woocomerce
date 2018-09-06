c2ee2d5370fdfbb3e683ec852c9b672d2f6bd2a6
Nguyen Vinh <thevinh1841996@gmail.com>
2018-09-06 14:27:10 +0700

    06.9.2018 2

--- wp-content/themes/Epal-theme/functions.php
+++ wp-content/themes/Epal-theme/functions.php
@@ -49,8 +49,57 @@ function dvb_custom_mime_types($mimes) {
 add_filter('upload_mimes', 'dvb_custom_mime_types');
 
 
+//Add Css Thay Đổi Trang wp-login wordpress
+function login_css() {
+    wp_enqueue_style( 'login_css', get_template_directory_uri() . '/wp-admin/login.css' );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+}
+add_action('login_head', 'login_css');
 
 
+
+// Hide Admin User
+add_action('pre_user_query','yoursite_pre_user_query');
+function yoursite_pre_user_query($user_search) {
+   global $current_user;
+   $username = $current_user->user_login;
+
+   if ($username == 'Epal') {
+     global $wpdb;
+     $user_search->query_where = str_replace('WHERE 1=1',
+       "WHERE 1=1 AND {$wpdb->users}.user_login != 'Epal'",$user_search->query_where);
+ }
+}
+
+
+//Thêm Font size vào text Editor
+function scanwp_buttons( $buttons ) {
+    array_unshift( $buttons, 'fontsizeselect' ); 
+    return $buttons;
+}
+add_filter( 'mce_buttons_2', 'scanwp_buttons' );
+function scanwp_font_size( $initArray ){
+    $initArray['fontsize_formats'] = "9px 10px 11px 12px 13px 14px 15px 16px 17px 18px 19px 20px";
+    return $initArray;
+}
+add_filter( 'tiny_mce_before_init', 'scanwp_font_size' );
+
+
+//Wedget
+if (function_exists('register_sidebar')){
+    register_sidebar(array(
+        'name'=> 'Sidebar',
+        'id' => 'sidebar',
+    ));
+}
+
+
+// Ẩn Menu Admin
+// function chetz_remove_admin_menus(){
+// if ( function_exists('remove_menu_page') ) { 
+
+//     remove_menu_page( 'plugins.php' ); 
+// }}add_action('admin_menu', 'chetz_remove_admin_menus');
 
 
 //404, phai vao tao 1 trang 404 trong wordpress
@@ -64,12 +113,11 @@ function redirect_404_to_homepage() {
 }
 
 
-
-
 //Add Template Woocommerce
 $storefront = (object) array(
     'main'       => require 'inc/class-storefront.php',
 );
+
 
 //Code phan trang
 function devvn_wp_corenavi($custom_query = null, $paged = null) {
@@ -93,19 +141,14 @@ function devvn_wp_corenavi($custom_query = null, $paged = null) {
 }
 
 
-
 // Tối ưu Woocommerce css, js
 add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
 
 function child_manage_woocommerce_styles()
 {
-//remove generator meta tag
     remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
-
-    //first check that woo exists to prevent fatal errors
     if ( function_exists( 'is_woocommerce' ) )
     {
-        //dequeue scripts and styles
         if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() )
         {
             wp_dequeue_style( 'woocommerce_frontend_styles' );
@@ -131,3 +174,11 @@ function child_manage_woocommerce_styles()
         }
     }
 }
+
+
+
+
+
+
+
+
